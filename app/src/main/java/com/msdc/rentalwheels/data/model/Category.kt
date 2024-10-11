6c236a7c755232b53fbcf48a9e498fb3d6223ad3
muchaisam <samson.muchai@strathmore.edu>
2024-10-11 13:58:10 +0300

    Add Category data model class

---
+++ app/src/main/java/com/msdc/rentalwheels/data/model/Category.kt
@@ -0,0 +1,8 @@
+package com.msdc.rentalwheels.data.model
+
+data class Category(
+    val id: String = "",
+    val name: String = "",
+    val imageUrl : String = "",
+    val description: String = "",
+)
