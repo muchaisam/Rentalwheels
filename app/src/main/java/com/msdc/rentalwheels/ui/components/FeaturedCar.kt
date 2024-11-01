10bccbe7f9bd3802e1f9eb4294e7c00ee3bdf074
muchaisam <samson.muchai@strathmore.edu>
2024-11-01 10:24:15 +0300

    feat(featured-car): Update FeaturedCar component to use custom Typography and adjust image dimensions

--- app/src/main/java/com/msdc/rentalwheels/ui/components/FeaturedCar.kt
+++ app/src/main/java/com/msdc/rentalwheels/ui/components/FeaturedCar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,7 +14,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import com.msdc.rentalwheels.ui.theme.Typography
 import coil.request.ImageRequest
 import com.msdc.rentalwheels.data.model.Car
 
@@ -22,26 +23,26 @@ fun FeaturedCar(car: Car) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(300.dp)
+            .height(320.dp)
     ) {
         Column {
-            AsyncImage(
+            ShimmerAsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(car.imageUrl)
                     .crossfade(true)
                     .build(),
                 contentDescription = "${car.brand} ${car.model}",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp),
+                    .width(220.dp)
+                    .height(220.dp),
                 contentScale = ContentScale.Crop
             )
             Column(modifier = Modifier.padding(16.dp)) {
-                Text("Featured: ${car.brand} ${car.model}", style = MaterialTheme.typography.titleLarge)
-                Text(car.description, style = MaterialTheme.typography.bodyMedium, maxLines = 2, overflow = TextOverflow.Ellipsis)
+                Text("Featured: ${car.brand} ${car.model}", style = Typography.titleLarge)
+                Text(car.description, style = Typography.bodyMedium, maxLines = 2, overflow = TextOverflow.Ellipsis)
                 Text(
                     "Ksh ${car.dailyRate}/Day",
-                    style = MaterialTheme.typography.titleMedium,
+                    style = Typography.titleMedium,
                     color = MaterialTheme.colorScheme.primary
                 )
             }
