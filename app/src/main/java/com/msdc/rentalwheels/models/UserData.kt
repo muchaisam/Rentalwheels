2c1ff6b4d32cda460eeb922973f217aca02ac008
muchaisam <samson.muchai@strathmore.edu>
2024-11-01 10:18:08 +0300

    feat(models): Add UserData data class for user information storage

---
+++ app/src/main/java/com/msdc/rentalwheels/models/UserData.kt
@@ -0,0 +1,8 @@
+package com.msdc.rentalwheels.models
+
+data class UserData(
+    val email: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val phoneNumber: String = ""
+)
