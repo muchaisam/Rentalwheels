256077ff70d372f189d7f4a9206d5fe6714915a7
muchaisam <samson.muchai@strathmore.edu>
2024-11-01 10:26:25 +0300

    feat(theme): Add ThemeState class for managing and toggling theme modes

---
+++ app/src/main/java/com/msdc/rentalwheels/ui/theme/ThemeState.kt
@@ -0,0 +1,19 @@
+package com.msdc.rentalwheels.ui.theme
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import com.msdc.rentalwheels.ui.theme.ThemeMode
+
+class ThemeState(
+    initialThemeMode: ThemeMode = ThemeMode.Light
+) {
+    private val _themeMode = mutableStateOf(initialThemeMode)
+    val themeMode: State<ThemeMode> = _themeMode
+
+    fun toggleTheme() {
+        _themeMode.value = when (themeMode.value) {
+            ThemeMode.Light -> ThemeMode.Dark
+            ThemeMode.Dark -> ThemeMode.Light
+        }
+    }
+}
