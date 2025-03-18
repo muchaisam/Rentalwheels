354fa2ad8a7563c470f68cf8e9e8373eb58e0b17
muchaisam <samson.muchai@strathmore.edu>
2025-03-18 22:18:41 +0300

    refactor(ui): Simplify imports and enhance layout in SettingsItem component

--- app/src/main/java/com/msdc/rentalwheels/ui/components/settings/SettingsItem.kt
+++ app/src/main/java/com/msdc/rentalwheels/ui/components/settings/SettingsItem.kt
@@ -1,14 +1,19 @@
 package com.msdc.rentalwheels.ui.components.settings
 
-import androidx.compose.foundation.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.msdc.rentalwheels.ui.theme.Typography
