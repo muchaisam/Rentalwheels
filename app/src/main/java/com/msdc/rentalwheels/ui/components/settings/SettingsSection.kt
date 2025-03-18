107e3ab4ed2f0e171fdcf8c25ecc6b39c9accad7
muchaisam <samson.muchai@strathmore.edu>
2025-03-18 22:19:05 +0300

    refactor(ui): Update imports in SettingsSection component for clarity

--- app/src/main/java/com/msdc/rentalwheels/ui/components/settings/SettingsSection.kt
+++ app/src/main/java/com/msdc/rentalwheels/ui/components/settings/SettingsSection.kt
@@ -1,7 +1,8 @@
 package com.msdc.rentalwheels.ui.components.settings
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
