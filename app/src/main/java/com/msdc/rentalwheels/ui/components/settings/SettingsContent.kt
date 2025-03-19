5bc0bd565ff793b393b9436778e5732406209d42
muchaisam <samson.muchai@strathmore.edu>
2025-03-19 19:54:28 +0300

    refactor(ui): Revise SettingsContent component with improved layout and imports

--- app/src/main/java/com/msdc/rentalwheels/ui/components/settings/SettingsContent.kt
+++ app/src/main/java/com/msdc/rentalwheels/ui/components/settings/SettingsContent.kt
@@ -1,22 +1,32 @@
 package com.msdc.rentalwheels.ui.components.settings
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.runtime.*
-import com.msdc.rentalwheels.models.UserData
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.*
-import androidx.compose.ui.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.*
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.*
+import androidx.compose.ui.unit.dp
+import com.msdc.rentalwheels.models.UserData
 import com.msdc.rentalwheels.ui.theme.Typography
 
 @Composable
