fe743e7b433e41c2c17dce09e0f759d6d2177643
muchaisam <samson.muchai@strathmore.edu>
2025-03-22 14:05:57 +0300

    refactor(repository): Remove unused Log import from CarRepository

--- app/src/main/java/com/msdc/rentalwheels/data/repository/CarRepository.kt
+++ app/src/main/java/com/msdc/rentalwheels/data/repository/CarRepository.kt
@@ -1,6 +1,5 @@
 package com.msdc.rentalwheels.data.repository
 
-import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.msdc.rentalwheels.data.model.Car
