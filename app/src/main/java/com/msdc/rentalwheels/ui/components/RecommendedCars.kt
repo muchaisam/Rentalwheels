b2222d910025f913c53acdf5794c95cd5246b3bc
muchaisam <samson.muchai@strathmore.edu>
2025-03-22 14:16:51 +0300

    refactor(ui): Update RecommendedCars component layout by adjusting padding and removing unused imports

--- app/src/main/java/com/msdc/rentalwheels/ui/components/RecommendedCars.kt
+++ app/src/main/java/com/msdc/rentalwheels/ui/components/RecommendedCars.kt
@@ -5,22 +5,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
-import com.msdc.rentalwheels.ui.theme.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.msdc.rentalwheels.data.model.Car
+import com.msdc.rentalwheels.ui.theme.Typography
 
 @Composable
 fun RecommendedCars(cars: List<Car>) {
@@ -28,7 +24,7 @@ fun RecommendedCars(cars: List<Car>) {
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()  // Changed from fixed height
-            .padding(8.dp)
+            .padding(vertical = 8.dp)
     ) {
         Text("Recommended Cars", style = Typography.titleLarge)
         Spacer(modifier = Modifier.height(16.dp))
