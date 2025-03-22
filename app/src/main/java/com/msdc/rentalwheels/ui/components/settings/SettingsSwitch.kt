c5a64905ed446302b175aa5ce75c0e200d6b6786
muchaisam <samson.muchai@strathmore.edu>
2025-03-22 10:10:11 +0300

    fix(ui): Import missing Alignment and Modifier for SettingsSwitch component

--- app/src/main/java/com/msdc/rentalwheels/ui/components/settings/SettingsSwitch.kt
+++ app/src/main/java/com/msdc/rentalwheels/ui/components/settings/SettingsSwitch.kt
@@ -7,7 +7,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
