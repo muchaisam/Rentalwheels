1f7c99491875ea1950e72031799a193c3f534c69
muchaisam <samson.muchai@strathmore.edu>
2025-03-22 14:09:57 +0300

    feat(navigation): Update DetailedCarScreen route to include carId parameter for dynamic navigation

--- app/src/main/java/com/msdc/rentalwheels/appnavigation/Screen.kt
+++ app/src/main/java/com/msdc/rentalwheels/appnavigation/Screen.kt
@@ -3,7 +3,9 @@ package com.msdc.rentalwheels.appnavigation
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Browse : Screen("search")
-    object DetailedCarScreen : Screen("car_detail")
+    object DetailedCarScreen : Screen("car_detail/{carId}") {
+        fun createRoute(carId: String) = "car_detail/$carId"
+    }
     object Bookings : Screen("library")
     object Settings : Screen("settings")
 }
