f866a70629550b30412c9615c79b3fa82260d940
muchaisam <samson.muchai@strathmore.edu>
2025-06-19 21:41:16 +0300

    refactor(theme): Simplify ThemeState implementation by using mutableStateOf for themeMode

--- app/src/main/java/com/msdc/rentalwheels/ui/theme/ThemeState.kt
+++ app/src/main/java/com/msdc/rentalwheels/ui/theme/ThemeState.kt
@@ -1,18 +1,23 @@
 package com.msdc.rentalwheels.ui.theme
 
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-class ThemeState(
-    initialThemeMode: ThemeMode = ThemeMode.Light
-) {
-    private val _themeMode = mutableStateOf(initialThemeMode)
-    val themeMode: State<ThemeMode> = _themeMode
+class ThemeState(initialTheme: ThemeMode = ThemeMode.Light) {
+    var themeMode by mutableStateOf(initialTheme)
+        private set
 
     fun toggleTheme() {
-        _themeMode.value = when (themeMode.value) {
-            ThemeMode.Light -> ThemeMode.Dark
-            ThemeMode.Dark -> ThemeMode.Light
-        }
+        themeMode =
+            when (themeMode) {
+                ThemeMode.Light -> ThemeMode.Dark
+                ThemeMode.Dark -> ThemeMode.Light
+            }
+    }
+
+    fun setTheme(mode: ThemeMode) {
+        themeMode = mode
     }
 }
