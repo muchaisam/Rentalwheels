53accb60aae72a3e55cf4a7e100c8d22e5256599
muchaisam <samson.muchai@strathmore.edu>
2025-07-09 13:15:54 +0300

    feat(rememberThemeState): Update theme state management to use saved preferences

--- app/src/main/java/com/msdc/rentalwheels/ui/theme/rememberThemeState.kt
+++ app/src/main/java/com/msdc/rentalwheels/ui/theme/rememberThemeState.kt
@@ -1,12 +1,21 @@
 package com.msdc.rentalwheels.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
-fun rememberThemeState(
-    initialTheme: ThemeMode = if (isSystemInDarkTheme()) ThemeMode.Dark else ThemeMode.Light
-): ThemeState {
-    return remember { ThemeState(initialTheme) }
+fun rememberThemeState(initialTheme: ThemeMode = ThemeMode.System): ThemeState {
+    val context = LocalContext.current
+    val themePreferencesManager = remember { ThemePreferencesManager(context) }
+    val savedPreference = remember { themePreferencesManager.getThemePreference() }
+
+    val themeMode =
+        when (savedPreference) {
+            ThemePreference.LIGHT -> ThemeMode.Light
+            ThemePreference.DARK -> ThemeMode.Dark
+            ThemePreference.SYSTEM_DEFAULT -> ThemeMode.System
+        }
+
+    return remember { ThemeState(themeMode) }
 }
