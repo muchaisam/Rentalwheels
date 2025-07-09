e7056d78313f3ae7538769129a1999dc70d99762
muchaisam <samson.muchai@strathmore.edu>
2025-07-09 12:35:59 +0300

    refactor(screen): Add BookingDetail and Analytics screens to navigation

--- app/src/main/java/com/msdc/rentalwheels/appnavigation/Screen.kt
+++ app/src/main/java/com/msdc/rentalwheels/appnavigation/Screen.kt
@@ -6,6 +6,12 @@ sealed class Screen(val route: String) {
     object DetailedCarScreen : Screen("car_detail/{carId}") {
         fun createRoute(carId: String) = "car_detail/$carId"
     }
+
     object Bookings : Screen("library")
+    object BookingDetail : Screen("booking_detail/{bookingId}") {
+        fun createRoute(bookingId: String) = "booking_detail/$bookingId"
+    }
+
+    object Analytics : Screen("analytics")
     object Settings : Screen("settings")
 }
